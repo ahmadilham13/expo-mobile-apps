05a5c4ac5e38ada1de59d53eef3ba6259b8a8b1e
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-11-14 04:03:16 +0700

    Update: bottom tab navigator with icon

---
+++ src/assets/icon/index.jsx
@@ -0,0 +1,9 @@
+import IconHome from './home.jsx'
+import IconHomeActive from './homeActive.jsx'
+import IconPesanan from './pesanan.jsx'
+import IconPesananActive from './PesananActive.jsx'
+import IconAkun from './akun.jsx'
+import IconAkunActive from './akunActive.jsx'
+
+
+export {IconHome, IconHomeActive, IconPesanan, IconPesananActive, IconAkun, IconAkunActive}
---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+import BottomNavigator from "./BottomNavigatior";
+
+export {BottomNavigator}
